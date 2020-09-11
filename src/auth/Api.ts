6038ec602cc597a137f3358a4bd4331586f01b00
KubaWolanin-at-776469355126 <Jakub19000@gmail.com>
2020-09-11 21:20:21 +0200

    Add default configuration to axios

--- src/auth/Api.ts
+++ src/auth/Api.ts
@@ -1,12 +1,11 @@
 import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { CredentialsDto } from '../interfaces/dto/credentials-dto.interface';
 import { TokenRo } from '../interfaces/ro/token-ro.interface';
-import { API } from '../env';
 
 export const login = async (data: CredentialsDto): Promise<AxiosResponse<TokenRo>> => {
     const requestConfig: AxiosRequestConfig = {
         method: 'post',
-        url: `${API}/auth/authenticate`,
+        url: `/auth/authenticate`,
         data
     }
 
