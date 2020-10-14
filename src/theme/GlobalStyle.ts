79ace86a4057fc74e1689538e23718b8999802b0
Pawel Jaworski <javorski.pavel@gmail.com>
2020-10-14 14:36:10 +0200

    globalstyle import

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -1,9 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap');
 *, *::before, *::after {
     box-sizing: border-box; 
-    
+    font-family: 'Lato';
 }
 
 html {
